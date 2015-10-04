b456321f09b441c021afe0682d8622e90167dc81
skillbr <fabiano_m@live.com>
2015-10-04 15:25:24 -0300

    metodo tela criado

--- JogoDaVelha/src/com/grupoprogramacaoej/skill.java
+++ JogoDaVelha/src/com/grupoprogramacaoej/skill.java
@@ -1,5 +1,28 @@
 package com.grupoprogramacaoej;
 
 public class skill {
-
+	
+	String posicoes [] = new String[9];
+	
+	public void tela(){
+		
+		System.out.println(posicoes[0]+""+posicoes[1]+""+posicoes[2]);	
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	skill tela = new skill();	
+		
+	for (int i = 0; i < 9; i++) {
+		
+		tela.posicoes[i] = ""+(i+1);
+		
+			
+		}	
+		
+		
+	}
+			
 }
