fab2bc3012377c9fb1218043ee5df2a8275dd294
WIN7 <WIN7@WIN7-PC>
2015-10-04 18:49:29 -0300

    Eu

--- JogoDaVelha/src/com/grupoprogramacaoej/Ulisses.java
+++ JogoDaVelha/src/com/grupoprogramacaoej/Ulisses.java
@@ -1,57 +1,62 @@
-
-			
 package com.grupoprogramacaoej;
 
 import java.util.Scanner;
 
 public class Ulisses {
-
-	String posicoes[] = new String[9];
 	
-	public void tela () {
-		System.out.println(posicoes[0]+" "+posicoes[1]+" "+posicoes[2]);
-		System.out.println(posicoes[3]+" "+posicoes[4]+" "+posicoes[5]);
-		System.out.println(posicoes[6]+" "+posicoes[7]+" "+posicoes[8]);
+	String posicoes [] = new String[9];
+	
+	//metodo tela criado...
+	public void tela(){
+		
+		System.out.println(posicoes[0]+""+posicoes[1]+""+posicoes[2]);	
+		System.out.println(posicoes[3]+""+posicoes[4]+""+posicoes[5]);
+		System.out.println(posicoes[6]+""+posicoes[7]+""+posicoes[8]);
 	}
 	
 	
-	public static void main(String[] args) {	
-
-		Ulisses matriz = new Ulisses();
-		String player1, player2, escolha,nomeprimeirojogador,nomesegundojogador;
-		int cont1 = 0, cont2 = 0;
-		Scanner leitura = new Scanner(System.in);
+	public static void main(String[] args) {
+	
+	System.out.println("-----------------------------------------------");
+	System.out.println("    BEM-VINDO AO JOGO DA VELHA VERSÃO 1.0");
+	System.out.println("-----------------------------------------------");
+	System.out.println("                                               ");
+	
+		// nome da classe + nome qualquer = new + nome da classe();	
+	Ulisses matriz = new Ulisses();	
+	
+	String Player1, Player2, Escolha,nomeprimeirojogador,nomesegundojogador;
+	int cont1 = 0 , cont2 = 0;
+	
+	Scanner leitura = new Scanner (System.in);
+	
+	for (int i = 0; i < 9; i++) {
 		
-		for (int i = 0; i < 9; i++) {
-			matriz.posicoes[i] = ""+(i+1);
-		}
+		matriz.posicoes[i] = ""+(i+1)+"  ";
+		}	
 		
-		System.out.println("Bem-vindo ao Jogo da Velha");
-		matriz.tela();
-		System.out.println("Qual nome do primeiro jogador ?");
-		nomeprimeirojogador = leitura.nextLine();
-		System.out.println("Qual nome do segundo jogador ?");
-		nomesegundojogador = leitura.nextLine();
-		
-		
-		//sómente X ou O
-		
-		System.out.println(nomeprimeirojogador+" deseja jogar com X ou O: ");
-		escolha = leitura.nextLine();
-		
-		while (!(escolha.equalsIgnoreCase("X") || escolha.equalsIgnoreCase("O"))) {
+	System.out.println("Qual nome do primeiro jogador ?");
+	nomeprimeirojogador = leitura.nextLine();
+	System.out.println("Qual nome do segundo jogador ?");
+	nomesegundojogador = leitura.nextLine();
+	
+	System.out.println("Olá "+nomeprimeirojogador+" você deseja jogar com [X] ou com [O]?");
+	Escolha = leitura.nextLine();
+	
+	while (!(Escolha.equalsIgnoreCase("X") || Escolha.equalsIgnoreCase("O"))) {
 		System.out.println(nomeprimeirojogador+" Escolha X ou O");
-		escolha = leitura.nextLine();
+		Escolha = leitura.nextLine();
 		}
-		
-		if (escolha.equalsIgnoreCase("X")) {
-			player1 = "X";
-			player2 = "O";
+
+		if (Escolha.equalsIgnoreCase("X")) {
+			Player1 = "X";
+			Player2 = "O";	
 		} else {
-			player1 = "O";
-			player2 = "X";
-		}
 		
+			Player1 = "O";
+			Player2 = "X";
+		}
+	
 		while ( (!(matriz.posicoes[0].equals(matriz.posicoes[1]) && matriz.posicoes[1].equals(matriz.posicoes[2]))) &&  
 				(!(matriz.posicoes[3].equals(matriz.posicoes[4]) && matriz.posicoes[4].equals(matriz.posicoes[5]))) &&
 				(!(matriz.posicoes[6].equals(matriz.posicoes[7]) && matriz.posicoes[7].equals(matriz.posicoes[8]))) &&
@@ -59,59 +64,74 @@ public class Ulisses {
 				(!(matriz.posicoes[1].equals(matriz.posicoes[4]) && matriz.posicoes[4].equals(matriz.posicoes[7]))) &&
 				(!(matriz.posicoes[2].equals(matriz.posicoes[5]) && matriz.posicoes[5].equals(matriz.posicoes[8]))) &&
 				(!(matriz.posicoes[0].equals(matriz.posicoes[4]) && matriz.posicoes[4].equals(matriz.posicoes[8]))) &&
-				(!(matriz.posicoes[2].equals(matriz.posicoes[4]) && matriz.posicoes[4].equals(matriz.posicoes[6])))	&&
+				(!(matriz.posicoes[2].equals(matriz.posicoes[4]) && matriz.posicoes[4].equals(matriz.posicoes[6])))	&&			
 				!(cont1 == 5)
-				) {
-
+				)
+				
+				{
+			
+			matriz.tela();
+			
 			if (cont1 <= cont2) {
-				
-				System.out.println(nomeprimeirojogador+" informe a posição que deseja marcar");
-				escolha = leitura.nextLine();
-				
-				while (matriz.posicoes[Integer.parseInt(escolha)-1].equalsIgnoreCase("X") || 
-						matriz.posicoes[Integer.parseInt(escolha)-1].equalsIgnoreCase("O")) {
-				System.out.println("Informe a posição que deseja marcar");
-				escolha = leitura.nextLine();
-				}
-				
-				matriz.posicoes[Integer.parseInt(escolha)-1] = player1;
-				matriz.tela();
-				cont1++;
-			} else {
-				
-				System.out.println(nomesegundojogador+ " Informe a posição que deseja marcar");
-				escolha = leitura.nextLine();
-				
-				while (matriz.posicoes[Integer.parseInt(escolha)-1].equalsIgnoreCase("X") || 
-						matriz.posicoes[Integer.parseInt(escolha)-1].equalsIgnoreCase("O")) {
-				System.out.println("Informe a posição que deseja marcar");
-				escolha = leitura.nextLine();
-				}
-				
-				matriz.posicoes[Integer.parseInt(escolha)-1] = player2;
-				matriz.tela();
-				cont2++;
+			
+			cont1++;
+			
+			System.out.println("                                   ");
+			System.out.println(nomeprimeirojogador+" informe a posição que deseja marcar");
+			Escolha = leitura.nextLine();
+			
+			//Se o espaço já estiver preenchido ele repete a pergunta
+			while (matriz.posicoes[Integer.parseInt(Escolha)-1].equalsIgnoreCase("X") || 
+					matriz.posicoes[Integer.parseInt(Escolha)-1].equalsIgnoreCase("O")) {
+			System.out.println(nomeprimeirojogador+" informe a posição que deseja marcar");
+			Escolha = leitura.nextLine();
 			}
-		}
+			
+			
+			matriz.posicoes[Integer.parseInt(Escolha)-1] = Player1+"  ";
+			
+			
+			}else
+			
+			{
+			cont2++;	
+			System.out.println(nomesegundojogador+" informe a posição que deseja marcar");
+			Escolha = leitura.nextLine();
+		
+			//Se o espaço já estiver preenchido ele repete a pergunta
+			while (matriz.posicoes[Integer.parseInt(Escolha)-1].equalsIgnoreCase("X") || 
+					matriz.posicoes[Integer.parseInt(Escolha)-1].equalsIgnoreCase("O")) {
+			System.out.println(nomesegundojogador+" informe a posição que deseja marcar");
+			Escolha = leitura.nextLine();
+			}
+			
+			
+			matriz.posicoes[Integer.parseInt(Escolha)-1] = Player2+ "  ";
+			
+			
+			}
+					
+		
+}
+		
+		matriz.tela();
 		
 		if (cont1 == 5) {
 			System.out.println("-----------------------------");
-			System.out.println("	     Deu Velha!!		 ");
+			System.out.println("	    Deu Velha!!		 ");
 			System.out.println("-----------------------------");
 		} else {
 			if (cont1 > cont2) {
-				System.out.println("-----------------------------");
-				System.out.println("Parabéns "+nomeprimeirojogador+" Ganhou a Partida!");
-				System.out.println("-----------------------------");
+				System.out.println("-----------------------------------------------------");
+				System.out.println("Parabéns "+ nomeprimeirojogador+ " você ganhou a partida!");
+				System.out.println("-----------------------------------------------------");
 			} else {
 
-				System.out.println("-----------------------------");
-				System.out.println("Parabéns "+nomesegundojogador+" Ganhou a Partida!");
-				System.out.println("-----------------------------");
+				System.out.println("-----------------------------------------------------");
+				System.out.println("Parabéns "+ nomesegundojogador+ " você ganhou a partida!");
+				System.out.println("-----------------------------------------------------");
 			}
 		}
-		
-		
+			
 	}
-
 }
