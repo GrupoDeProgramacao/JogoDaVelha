d98a92a1deb15ecd11ca88a2b03380015b40702c
skillbr <fabiano_m@live.com>
2015-10-04 15:45:24 -0300

    player 1 e 2

--- JogoDaVelha/src/com/grupoprogramacaoej/skill.java
+++ JogoDaVelha/src/com/grupoprogramacaoej/skill.java
@@ -1,5 +1,7 @@
 package com.grupoprogramacaoej;
 
+import java.util.Scanner;
+
 public class skill {
 	
 	String posicoes [] = new String[9];
@@ -8,21 +10,38 @@ public class skill {
 	public void tela(){
 		
 		System.out.println(posicoes[0]+""+posicoes[1]+""+posicoes[2]);	
-		
+		System.out.println(posicoes[3]+""+posicoes[4]+""+posicoes[5]);
+		System.out.println(posicoes[6]+""+posicoes[7]+""+posicoes[8]);
 	}
 	
 	
 	public static void main(String[] args) {
-		
-	skill tela = new skill();	
-		
+	
+	System.out.println("-----------------------------------------------");
+	System.out.println("    BEM-VINDO AO JOGO DA VELHA VERSÃO 1.0");
+	System.out.println("-----------------------------------------------");
+	System.out.println("                                               ");
+	
+		// nome da classe + nome qualquer = new + nome da classe();	
+	skill matriz = new skill();	
+	
+	String Player1 ;
+	String Player2 ;
+	
+	Scanner leitura = new Scanner (System.in);
+	
+	System.out.println("Olá player [1] deseja jogar com [X] ou com [O]?");
+	
+	
+	
 	for (int i = 0; i < 9; i++) {
 		
-		tela.posicoes[i] = ""+(i+1);
+		matriz.posicoes[i] = ""+(i+1)+"  ";
 		
-			
-		}	
+			}	
 		
+	
+	matriz.tela();
 		
 	}
 			
