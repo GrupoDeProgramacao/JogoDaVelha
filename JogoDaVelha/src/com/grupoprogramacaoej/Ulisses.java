125bd93c33e55588d7765c910c15353d2d974c23
WIN7 <WIN7@WIN7-PC>
2015-10-04 17:33:52 -0300

    Implementação nomes jogadores

--- JogoDaVelha/src/com/grupoprogramacaoej/Ulisses.java
+++ JogoDaVelha/src/com/grupoprogramacaoej/Ulisses.java
@@ -17,9 +17,10 @@ public class Ulisses {
 	public static void main(String[] args) {	
 
 		Ulisses matriz = new Ulisses();
-		String player1, player2, escolha;
+		String player1, player2, escolha,nomeprimeirojogador,nomesegundojogador;
 		player1 = "X";
 		player2 = "O";
+		int cont1 = 0, cont2 = 0;
 		
 		Scanner leitura = new Scanner (System.in);
 		
@@ -31,9 +32,12 @@ public class Ulisses {
 		
 		System.out.println ("BEM VINDO AO JOGA DA VELHA ");
 		matriz.tela();
-		
-		System.out.println("Deseja jogar com X ou O: ");
-		escolha = leitura.nextLine();
+		System.out.println ("Qual nome do primeiro jogador ? ");
+			nomeprimeirojogador = leitura.nextLine();
+		System.out.println ("Qual nome do segundo jogador ? ");
+			nomesegundojogador = leitura.nextLine();
+		System.out.println(""+nomeprimeirojogador+ " você deseja jogar com X ou O ?");
+			escolha = leitura.nextLine();
 		
 		if (escolha.equalsIgnoreCase("X")) {
 			player1 = "X";
@@ -42,21 +46,32 @@ public class Ulisses {
 			player1 = "X";
 			player2 = "O";
 		}
-		while (!(matriz.posicoes[0].equals(matriz.posicoes[1]) && matriz.posicoes[1].equals(matriz.posicoes[2])) &&
-			  !(matriz.posicoes[3].equals(matriz.posicoes[4]) && matriz.posicoes[4].equals(matriz.posicoes[5])) && 
-			  !(matriz.posicoes[6].equals(matriz.posicoes[7]) && matriz.posicoes[7].equals(matriz.posicoes[8])) ) 
-		{
+		while ( (!(matriz.posicoes[0].equals(matriz.posicoes[1]) && matriz.posicoes[1].equals(matriz.posicoes[2]))) &&  
+				(!(matriz.posicoes[3].equals(matriz.posicoes[4]) && matriz.posicoes[4].equals(matriz.posicoes[5]))) &&
+				(!(matriz.posicoes[6].equals(matriz.posicoes[7]) && matriz.posicoes[7].equals(matriz.posicoes[8]))) &&
+				(!(matriz.posicoes[0].equals(matriz.posicoes[3]) && matriz.posicoes[3].equals(matriz.posicoes[5]))) &&
+				(!(matriz.posicoes[1].equals(matriz.posicoes[4]) && matriz.posicoes[4].equals(matriz.posicoes[7]))) &&
+				(!(matriz.posicoes[2].equals(matriz.posicoes[5]) && matriz.posicoes[5].equals(matriz.posicoes[8]))) &&
+				(!(matriz.posicoes[0].equals(matriz.posicoes[4]) && matriz.posicoes[4].equals(matriz.posicoes[8]))) &&
+				(!(matriz.posicoes[2].equals(matriz.posicoes[4]) && matriz.posicoes[4].equals(matriz.posicoes[8])))				
+				) {
 			
-			System.out.println("Informe a posição que deseja marcar");
-			escolha = leitura.nextLine();
-			matriz.posicoes[Integer.parseInt(escolha)-1] = player1;
-			matriz.tela();
-			System.out.println("Informe a posição que deseja marcar");
-			escolha = leitura.nextLine();
-			matriz.posicoes[Integer.parseInt(escolha)-1] = player2;
-			matriz.tela();
-			
+			if (cont1 <= cont2) {
+				
+				System.out.println("Informe a posição que deseja marcar " +nomeprimeirojogador);
+				escolha = leitura.nextLine();
+				matriz.posicoes[Integer.parseInt(escolha)-1] = player1;
+				matriz.tela();
+				cont1++;
+			} else {
+					System.out.println("Informe a posição que deseja marcar " +nomesegundojogador);
+					escolha = leitura.nextLine();
+					matriz.posicoes[Integer.parseInt(escolha)-1] = player2;
+					matriz.tela();
+					cont2++;
+			}
 		}
+		
 		
 		
 		
