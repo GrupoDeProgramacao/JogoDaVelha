978eafdff851b18036ba0c2dbe136c3d4e80d84d
™Rychwer™ <rychwer89@gmail.com>
2015-10-04 15:18:29 -0300

    testes

--- JogoDaVelha/src/com/grupoprogramacaoej/richer.java
+++ JogoDaVelha/src/com/grupoprogramacaoej/richer.java
@@ -2,9 +2,20 @@ package com.grupoprogramacaoej;
 
 public class richer {
 
-	public static void main(String[] args) {
+	String posicoes[] = new String[9];
+	
+	public void tela () {
+		System.out.println(posicoes[0]+""+posicoes[1]+""+posicoes[2]);
+	}
+	
+	
+	public static void main(String[] args) {	
 
+		richer matriz = new richer();
 		
+		for (int i = 0; i < 9; i++) {
+			matriz.posicoes[i] = ""+(i+1);
+		}
 		
 	}
 
