13de33f0d9ce2a695f5d9dd3fb2afafaa27e3b57
WIN7 <WIN7@WIN7-PC>
2015-10-11 15:06:55 -0300

    Códiogo

--- JogoDaVelha/src/com/grupoprogramacaoej/EstatisticaSimples.java
+++ JogoDaVelha/src/com/grupoprogramacaoej/EstatisticaSimples.java
@@ -1,10 +1,21 @@
 package com.grupoprogramacaoej;
 
+import java.util.Scanner;
+
 public class EstatisticaSimples {
 
 	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+    double valor_minimo, valor_maximo, valor_medio;
+    int numero_elementos;
+    int numero = 0;
+    
+    for (int i = 0; i < args.length; numero) {
 		
-
+	} {
+		
+	}
+    
 	}
 
 }
